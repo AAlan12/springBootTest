ebeda045a17533cd09ff81d7ae519162045e02cf
Antonio Alan <tonio.alan07@gmail.com>
2022-07-05 10:25:51 -0300

    simple database implementation

--- src/main/java/com/example/springboottutorial/entities/Category.java
+++ src/main/java/com/example/springboottutorial/entities/Category.java
@@ -2,6 +2,7 @@ package com.example.springboottutorial.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
 import java.awt.*;
 import java.io.Serial;
 import java.io.Serializable;
@@ -9,12 +10,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     @Serial
     private  static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
     private List<Product> products = new ArrayList<>();
 
     public Category(){
